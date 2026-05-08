a060d3268bfa22b37ac29da3b1b1657193872d15
José Emanuel RG <resendizgonzalezjoseemanuel@gmail.com>
2026-05-07 22:59:52 -0600

    Add: cambio de link render

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://localhost:7109',
+        target: 'https://career-copilot-api-tb5f.onrender.com',
         changeOrigin: true,
         secure: false,
       }
